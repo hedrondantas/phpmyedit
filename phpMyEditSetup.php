8d26dd6908f23b4dadbf5feba606dc52ffb9ebc6
Hedron <hedrondantas@gmail.com>
2022-01-19 13:07:26 -0300

    Add Bootstrap 5

--- phpMyEditSetup.php
+++ phpMyEditSetup.php
@@ -620,6 +620,7 @@ END;
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>$pageTitle</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 $css_directive
 </head>
 <body>
