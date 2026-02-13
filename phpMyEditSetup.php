d4df93ecc73e35492a2206315c4f2556e67a72fb
Hedron  Dantas <hedrondantas@gmail.com>
2026-02-12 22:57:22 -0300

    Add Bootstrap CSS and Bootstrap Icons
    Include Bootstrap 5.1.3 CSS and Bootstrap Icons 1.7.2 via CDN in phpMyEditSetup.php head to enable Bootstrap styling and iconography on the setup page. Integrity and crossorigin attributes were added for the Bootstrap CSS link; no functional PHP logic changes.

--- phpMyEditSetup.php
+++ phpMyEditSetup.php
@@ -40,6 +40,10 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
 	<title>phpMyEdit Setup</title>
+	 <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 	<style type="text/css">
 	<!--
 		body  { font-family: "Verdana", "Arial", "Sans-Serif"; text-align: left }
