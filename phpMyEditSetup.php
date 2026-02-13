5735d9e1071eaa48e6ef62a8003287d17c403c7c
Hedron  Dantas <hedrondantas@gmail.com>
2026-02-12 22:51:59 -0300

    Add Bootstrap CSS and icons CDN links
    Insert Bootstrap 5.1.3 CSS and Bootstrap Icons 1.7.2 CDN links into phpMyEditSetup.php head (after $css_directive). This ensures generated pages load Bootstrap styles and icon fonts; the Bootstrap link includes SRI integrity and crossorigin attributes.

--- phpMyEditSetup.php
+++ phpMyEditSetup.php
@@ -560,6 +560,10 @@ END;
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>$pageTitle</title>
 $css_directive
+ <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 </head>
 <body>
 $buffer
